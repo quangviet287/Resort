f4eaa2e1bd482c6e6806f987d35a9fd1dfe86aff
quangviet287 <quangviet287@gmail.com>
2018-01-02 10:36:50 +0700

    add name swimming pool

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,10 @@ public class Main {
         float noBed =vila.getBed()+h1.getBed();
         System.out.println("So giuong co trong Resort la: "+ noBed);
         SwimmingPool sw = new SwimmingPool();
+        sw.setName("Be boi 1");
         sw.setOpenTime("8:00 AM");
         sw.setEndTime("20:00 PM");
-        System.out.println("Thoi gian mo cua cua be boi la: " + sw.getHour());
+        System.out.println("Thoi gian mo cua cua "+sw.getName()+" la: " + sw.getHour());
 
         Tour t1 = new Tour();
         t1.setInfoTour("Da Nang","1000$","1/1/2018","1/15/2018");
