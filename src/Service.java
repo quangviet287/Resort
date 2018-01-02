101d3c94c0719a27a1c07d2de16fa1f12b1dd456
quangviet287 <quangviet287@gmail.com>
2018-01-02 10:46:15 +0700

    get set name

--- src/Service.java
+++ src/Service.java
@@ -2,5 +2,13 @@ abstract class Service {
     protected String name;
     protected String hour;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public abstract String getHour();
 }
